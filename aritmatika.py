2917e5c1c489b8cc9b470f15afc914d697e5ea2d
dikaa-bit <DIKARSKII90@GMAIL.COM>
2025-03-15 00:34:50 +0700

    Add files via upload

---
+++ aritmatika.py
@@ -0,0 +1,38 @@
+a = 5
+b = 3
+
+#penjumlahan(+)
+hasil = a + b
+print('Hasil Penjumlahan :',hasil)
+
+#pengurangan(-)
+hasil = a - b
+print('Hasil Pengurangan :',hasil)
+
+#perkalian(*)
+hasil = a * b
+print('Hasil perkalian :',hasil)
+
+#pangkat(**)
+hasil = a ** b
+print('Hasil pangkat :',hasil)
+
+#pembagian(/)
+hasil = a / b
+print('Hasil pembagian :',hasil)
+
+#pembagian kebawah(//)
+hasil = a // b
+print('Hasil pembagian kebawah :',hasil)
+
+#sisa bagi(%)
+hasil = a % b
+print('Hasil sisa bagi :',hasil)
+
+#precedence
+hasil = 1 + 2 * 4 - 3
+print('Hasil precedence :',hasil)
+
+#precedence
+hasil = (1 + 2) * 4 - 3
+print('Hasil precedence :',hasil)
---
+++ assignment.py
@@ -0,0 +1,14 @@
+#operator=
+a = 5
+b = 2
+
+#operator+=
+a += 3
+# a = a + 3
+
+#operator -=
+b -= 1
+# b = b - 1
+print(b)
+
+#operator ...
